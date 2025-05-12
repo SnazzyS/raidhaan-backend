28ba0f558cf5fa9dff57b1f44dcc600e3ae6bf5f
Mohamed Suhail <Suhail.1994@hotmail.com>
2025-05-12 16:21:04 +0500

    Add crud for category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Requests\CategoryRequest;
+use PDO;
 
 class CategoryController extends Controller
 {
@@ -14,9 +16,39 @@ class CategoryController extends Controller
         return response()->json($categories);
     }
 
+    public function store(CategoryRequest $request)
+    {
+
+        Category::create($request->validated());
+        
+        return response()->json([
+                'message' => 'Category created'
+            ], 201);
+
+    }
+
+    // not needed?
     public function show(Category $category)
     {
         return response()->json($category);
+    }
+
+    public function update(CategoryRequest $request, Category $category)
+    {
+        $category->update($request->validated());
+        
+        return response()->json([
+            'message' => 'Category updated successfully'
+        ]);
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return response()->json([
+            'message' => 'Category deleted successfully'
+        ], 200);
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,30 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Route::get('/category', CategoryController::class, 'index');
+Route::get('categories', [CategoryController::class, 'index']);
+Route::post('categories', [CategoryController::class, 'store']);
+Route::get('categories/{id}', [CategoryController::class, 'show']);
+Route::put('categories/{id}', [CategoryController::class, 'update']);
+Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+
+Route::get('items', [ItemController::class, 'index']);
+Route::post('items', [ItemController::class, 'store']);
+Route::get('items/{item}', [ItemController::class, 'show']);
+Route::put('items/{item}', [ItemController::class, 'update']);
+Route::delete('items/{item}', [ItemController::class, 'destroy']);
+
+Route::get('orders', [OrderController::class, 'index']);
+Route::post('orders', [OrderController::class, 'store']);
+Route::get('orders/{order}', [OrderController::class, 'show']);
+Route::put('orders/{order}', [OrderController::class, 'update']);
+Route::delete('orders/{order}', [OrderController::class, 'destroy']);
+Route::get('orders/{order}/receipt', [OrderController::class, 'receipt']);
