ce47afd5d8fd3dbf1d13c94dc925d7d0d2c8086f
Mohamed Suhail <Suhail.1994@hotmail.com>
2025-05-14 15:11:23 +0500

    Fix for sale index controller

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Sale;
 use Illuminate\Http\Request;
 use Spatie\QueryBuilder\QueryBuilder;
@@ -9,13 +10,20 @@ use Spatie\QueryBuilder\AllowedFilter;
 
 class SaleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $query = Sale::query();
+        
+        if ($request->filled('from') && $request->filled('to')) {
+            $from = Carbon::parse($request->from)->startOfDay();
+            $to = Carbon::parse($request->to)->endOfDay();
+            $query->whereBetween('created_at', [$from, $to]);
+        } else {
+            $query->whereDate('created_at', today());
+        }
 
-        $sales = QueryBuilder::for(Sale::class)
-        ->AllowedFilters([
-            AllowedFilter::exact('date'),
-        ])->get();
+        $sales = $query->get();
+
         
         return response()->json($sales);
     }
