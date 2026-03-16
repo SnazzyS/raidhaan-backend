2aa49bfe0ff5552c2d36efbe0fa15e92c4d53ed7
Mohamed Suhail <suhail.1994@hotmail.com>
2026-03-16 15:34:13 +0530

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title inertia>{{ config('app.name', 'Raidhaan') }}</title>
+    <link rel="icon" type="image/png" href="{{ \Illuminate\Support\Facades\Vite::asset('resources/js/assets/images/logo.png') }}">
+    <link rel="apple-touch-icon" href="{{ \Illuminate\Support\Facades\Vite::asset('resources/js/assets/images/logo.png') }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
